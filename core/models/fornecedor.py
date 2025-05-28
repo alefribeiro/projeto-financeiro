d94b5f681b0b59f72cf500b4bdf5ab14369e2538
Álef Ribeiro <alef.ribeiro@wlcsolucoes.com.br>
2025-05-28 08:03:22 -0300

    renomeia a classe FornecedorModel para Fornecedor e adiciona a classe TelefonesFornecedor

--- core/models/fornecedor.py
+++ core/models/fornecedor.py
@@ -15,7 +15,7 @@ class Cidade(models.Model):
     def __str__(self):
         return f'{self.nome} - {self.estado.uf}'
 
-class FornecedorModel(models.Model):
+class Fornecedor(models.Model):
     nome = models.CharField(max_length=100, blank=False, null=False)
     cnpj = models.CharField(max_length=14, blank=True, null=True)
     cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
@@ -23,7 +23,6 @@ class FornecedorModel(models.Model):
     numero = models.CharField(max_length=10, blank=True, null=True)
     complemento = models.CharField(max_length=255, blank=True, null=True)
     bairro = models.CharField(max_length=100, blank=True, null=True)
-    telefone = models.CharField(max_length=15, blank=False, null=False)
     cep = models.CharField(max_length=8, blank=True, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
     criado_em = models.DateTimeField(auto_now_add=True)
@@ -31,3 +30,10 @@ class FornecedorModel(models.Model):
 
     def __str__(self):
         return f'{self.nome} - {self.cnpj}'
+    
+class TelefonesFornecedor(models.Model):
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.PROTECT, related_name='telefones')
+    telefone = models.CharField(max_length=15, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.fornecedor.nome} - {self.telefone}'
